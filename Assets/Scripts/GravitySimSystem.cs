b60129318072df69ea0e51a97f5cdc5c4b07a0f1
Robin Hayes <robinh@pobox.com>
2019-11-01 10:15:52 -0700

    mega-checkin with lots of changes, most notably the sound and textures.

---
+++ Assets/Scripts/GravitySimSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+
+public class GravitySimSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref GS_velocity velocity, ref Translation position) =>
+        {
+            position.Value += velocity.velocity;
+        });
+    }
+}
